be0e278516835cde09cd15c127138b73f5cf86ba
Konstantin <varkac69@gmail.com>
2024-07-18 10:38:34 +0300

    added a few updates tomnavbar, header, footer components

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Footer() {
 	return (
-		<footer className="mt-16 flex flex-1 justify-between p-8 bg-white rounded-t-xl max-sm:rounded-none max-[450px]:flex-col max-[450px]:gap-4 max-[450px]:justify-around max-[450px]:items-center">
+		<footer className="mt-16 flex flex-1 shrink grow-0 justify-between p-8 bg-white rounded-t-xl max-sm:rounded-none max-[450px]:flex-col max-[450px]:gap-4 max-[450px]:justify-around max-[450px]:items-center">
 			<div>
 				<Link href="/" className="hover:underline">
 					Laptops by Sanya
